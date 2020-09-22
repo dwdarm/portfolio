e85bebe906c7f0cd434902b7f860e2c3c9d797fd
Fajar Dwi Darmanto <fajardwidarm@gmail.com>
2020-09-22 12:51:19 +0700

    restructuring sass

--- src/index.js
+++ src/index.js
@@ -4,22 +4,27 @@ import './styles/index.scss';
 const about = document.querySelector('.about');
 
 function animateHero() {
-  const title = document.querySelector('.hero-body h1');
-  const subtitle = document.querySelector('.hero-body p');
-  const scroll = document.querySelector('.hero-scroll');
+  const title = document.querySelector('.hero-body-title');
+  const subtitle = document.querySelector('.hero-body-subtitle');
+  const action = document.querySelector('.hero-body-action');
+  const scroll = document.querySelector('.hero-footer');
   title.style.opacity = 1;
   title.style.transform = 'translateX(0)';
   subtitle.style.opacity = 1;
   subtitle.style.transform = 'translateX(0)';
+  action.style.opacity = 1;
+  action.style.transform = 'translateX(0)';
   scroll.style.opacity = 1;
 }
 
-function scrollToAbout() {
+function scrollToAbout(event) {
+  event.preventDefault();
   about.scrollIntoView({ behavior: 'smooth' });
 }
 
 function initEvent() {
-  document.querySelector('.hero-scroll').onclick = scrollToAbout;
+  document.querySelector('.hero-body-action').onclick = scrollToAbout;
+  document.querySelector('.scroll-down-icon').onclick = scrollToAbout;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
